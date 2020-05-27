94d2c43395f02ca44eb169751fd5532aee90283a
AJ <7781450+ancheetah@users.noreply.github.com>
2020-05-27 11:56:29 -0400

    powershell version

--- file-append.sh
+++ file-append.sh
@@ -9,3 +9,12 @@ SET "Str=_IGEL"
 for %%a in ("%Src%\*.*") do ren "%%~a" "%%~Na%Str%%%~Xa
 
 #reference: https://superuser.com/questions/1349126/batch-append-to-end-of-a-file-name-before-the-extension-with-ren-command
+
+#-------------------------------------------------------------------------------------
+
+#Powershell syntax
+
+Get-ChildItem *JHA*.pdf | Rename-Item -NewName { $_.Name -replace '.pdf','_IGEL.pdf' }
+
+#the $_ automatic variable represents each file object
+#reference: https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/rename-item?view=powershell-7
