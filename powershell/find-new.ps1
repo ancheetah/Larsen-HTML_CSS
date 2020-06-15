b3ce3f254d153e548e916e6f0882371e0cec1943
AJ <7781450+ancheetah@users.noreply.github.com>
2020-06-15 17:19:24 -0400

    return to original working directory when done

--- powershell/find-new.ps1
+++ powershell/find-new.ps1
@@ -1,8 +1,10 @@
 #Checks for new files within a specified date range
-#CALL: & "$HOME\find-new.ps1"
+#EXAMPLE CALL: 	& "$HOME\find-new.ps1"	#from anywhere in command line
+#		find-new		#using alias set up in user profile
 
 #Check working directory
 Write-Host $pwd
+$oldpath = $pwd
 $choice = Read-Host "Is this the correct directory? (y/n) "
 if ($choice.ToLower() -eq "n") {
   $workingDir = Read-Host "Enter path"
@@ -28,6 +30,8 @@ $folders = @("11. Safety", "06. MOMs", "01. BOLs/06. Solar Modules", "05. EE Rep
 Get-ChildItem $folders -Recurse | Where-Object {$_.CreationTime -ge $startDate -and $_.CreationTime -le $endDate} | 
 	Format-Table -Property Name, [Blank], CreationTime -GroupBy Directory
 
+#Change back to user's original working directory
+Set-Location -LiteralPath $oldpath -PassThru
 Write-Host [Done]
 
 
