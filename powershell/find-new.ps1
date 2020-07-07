50dbe1b7ba7bd667d6ddd4cb2da288e3f0f671fb
AJ <7781450+ancheetah@users.noreply.github.com>
2020-07-07 17:03:55 -0400

    added comments for user

--- powershell/find-new.ps1
+++ powershell/find-new.ps1
@@ -7,13 +7,13 @@ Write-Host $pwd
 $oldpath = $pwd
 $choice = Read-Host "Is this the correct directory? (y/n) "
 if ($choice.ToLower() -eq "n") {
-  $workingDir = Read-Host "Enter path (no quotes): "
+  $workingDir = Read-Host "Enter path (no quotation marks) "
 }
 elseif ($choice.ToLower() -eq "y") {
 	$workingDir = $pwd
 }
 else {
-  Write-Host "Invalid entry... Aborting."
+  Write-Host "Invalid entry... Aborting"
   return -1
 }
 
@@ -21,13 +21,18 @@ Set-Location -LiteralPath $workingDir -PassThru
 
 #Prompt user for date-range
 $startDate = Read-Host "Enter start date"
-$endDate = Read-Host "Enter end date"	#Should be last date PlanGrid files are CREATED in ShareFile. Most likely the today's date.
+$endDate = Read-Host "Enter end date" 
+
+#Append time to list docs through end of day
+$endDate = $endDate + " 11:59:59 pm"
+#echo $endDate
 
 #Locations to check:
 $folders = @("11. Safety", "06. MOMs", "01. BOLs/06. Solar Modules", "05. EE Reports", "09. Photos - Progress")
 
 #List all files in date range
-Get-ChildItem $folders -Recurse | Where-Object {$_.CreationTime -ge $startDate -and $_.CreationTime -le $endDate} | 
+Get-ChildItem $folders -Recurse | 
+	Where-Object {$_.CreationTime -ge $startDate -and $_.CreationTime -le $endDate} | 
 	Format-Table -Property Name, [Blank], CreationTime -GroupBy Directory
 
 #Change back to user's original working directory
